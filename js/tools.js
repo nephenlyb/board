62e71423fdea16b890dfed1102bb29607733fcff
nephenlyb <pinglunxitong@163.com>
2019-05-11 16:14:14 +0800

    添加cookie

--- js/tools.js
+++ js/tools.js
@@ -82,8 +82,10 @@ $(document).on("click",".newBoardBtn",function(){
         $("#shorUrlInput").val('pages/board/board?title='+encodeURIComponent(title)+'&public='+public+'&image='+image);
         $("#coverImg").prop("src",image);
         $("#imgLookBtn").prop("href",image);
+        setCookie('username',public,365)
     } else if(image=='') {
         $("#shorUrlInput").val('pages/board/board?title='+encodeURIComponent(title)+'&public='+public);
+        setCookie('username',public,365)
     } else {
         alert('请输入一个正确的网址');
     }
@@ -172,4 +174,47 @@ function toolsXiuxiu(type,img,_XIUXIU) {
         area: ['1020px', '560px'],
         content:"/home/editor/meitu?img="+img+'&module='+type+'&type='+_XIUXIU
     });
+}
+
+function getCookie(c_name)
+{
+    if (document.cookie.length>0)
+    {
+        c_start=document.cookie.indexOf(c_name + "=")
+        if(c_start!=-1)
+        { 
+            c_start=c_start + c_name.length+1 
+            c_end=document.cookie.indexOf(";",c_start)
+            if(c_end==-1) c_end=document.cookie.length
+                return unescape(document.cookie.substring(c_start,c_end))
+            } 
+    }
+    return ""
+}
+
+function setCookie(c_name,value,expiredays)
+{
+    var exdate=new Date()
+    exdate.setDate(exdate.getDate()+expiredays)
+    document.cookie=c_name+ "=" +escape(value)+
+    ((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+}
+
+function checkCookie()
+{
+    username=getCookie('username')
+    if (username!=null && username!="")
+    {
+        alert('Welcome again '+username+'!')
+        $(".publicInputText").attr('value',username);
+        $(".publicInputText").attr('placeholder',"");
+    }
+    // else 
+    // {
+    //     username=prompt('请输入你的公众号名字:',"")
+    //     if(username!=null && username!="")
+    //     {
+    //         setCookie('username',username,365)
+    //     }
+    // }
 }
