471dd30a848d6c7d61a164fe0a8b2c9b473b326c
nephenlyb <pinglunxitong@163.com>
2019-05-12 11:45:31 +0800

    fix

--- js/kefu.js
+++ js/kefu.js
@@ -10,37 +10,6 @@
         var sideContent = document.querySelector(".side_content");
         var show_btn = document.querySelector(".show_btn");
         var close_btn = document.querySelector(".close_btn");
-        var timer = null;
-
-        //悬浮QQ匀速移动
-        var startMove = function(argument) {
-            var scrollsidebar = document.getElementById("scrollsidebar");
-            clearInterval(timer);
-            timer = setInterval(function() {
-                var speed = (argument - scrollsidebar.offsetTop) / 4;
-                speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-                if (argument == scrollsidebar.offsetTop) {
-                    clearInterval(timer);
-                } else {
-                    scrollsidebar.style.top = scrollsidebar.offsetTop + speed + "px";
-                }
-            }, 20);
-        };
-
-        //鼠标移动
-        var scrollMove = function() {
-            window.onscroll = window.onload = function() {
-                var scrollsidebar = document.getElementById("scrollsidebar");
-                var scrolltop =
-                    document.body.scrollTop || document.documentElement.scrollTop;
-                startMove(
-                    parseInt(
-                        (document.documentElement.clientHeight -
-                            scrollsidebar.offsetHeight) /2 +scrolltop
-                    )
-                );
-            };
-        };
 
         //悬浮QQ显示
         var slideShow = function() {
@@ -72,7 +41,6 @@
         //返回出来的方法
         return {
             init: function() {
-                scrollMove();
                 slideClose();
                 slideShow();
             }
@@ -82,3 +50,35 @@
     //初始化
     serviceOnline.init();
 })();
+
+//悬浮QQ匀速移动
+function startMove(argument) {
+    var timer = null;
+    var scrollsidebar = document.getElementById("scrollsidebar");
+    clearInterval(timer);
+    timer = setInterval(function() {
+        var speed = (argument - scrollsidebar.offsetTop) / 4;
+        speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+        if (argument == scrollsidebar.offsetTop) {
+            clearInterval(timer);
+        } else {
+            scrollsidebar.style.top = scrollsidebar.offsetTop + speed + "px";
+        }
+    }, 20);
+};
+
+//鼠标移动
+function scrollMove() {
+    // window.onscroll = function() {
+        console.log("scroll")
+        var scrollsidebar = document.getElementById("scrollsidebar");
+        var scrolltop =
+            document.body.scrollTop || document.documentElement.scrollTop;
+        startMove(
+            parseInt(
+                (document.documentElement.clientHeight -
+                    scrollsidebar.offsetHeight) /2 +scrolltop
+            )
+        );
+    // };
+};
